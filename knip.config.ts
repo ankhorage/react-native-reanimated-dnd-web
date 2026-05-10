93e45725b2067b7a934d98f8aa257d46aeb3ba55
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-05-11 00:09:55 +0200

    fix(ci): configure knip entries

--- knip.config.ts
+++ knip.config.ts
@@ -1,6 +1,11 @@
 import { createKnipConfig } from '@ankhorage/devtools/knip';
 
 export default createKnipConfig({
-  ignoreDependencies: [],
+  entry: ['examples/src/main.tsx'],
+  ignoreDependencies: [
+    'react-native',
+    'react-native-gesture-handler',
+    'react-native-reanimated',
+  ],
   ignoreFiles: ['.prettierrc.js', 'eslint.config.mjs'],
 });
